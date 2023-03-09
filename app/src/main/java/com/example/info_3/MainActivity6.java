e73bf4a8a3efc0e2349cb16a51568c0e6413bc6e
ifpr <anajulia.rdcastro@gmail.com>
2023-03-09 11:35:13 -0300

    aula 09/03/2023 projeto de botões

--- app/src/main/java/com/example/info_3/MainActivity6.java
+++ app/src/main/java/com/example/info_3/MainActivity6.java
@@ -2,13 +2,25 @@ package com.example.info_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity6 extends AppCompatActivity {
-
+    Button buttonvoltar5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+        buttonvoltar5=findViewById(R.id.buttonvoltar5);
+        buttonvoltar5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity6.this, MainActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/info_3/MainActivity7.java
+++ app/src/main/java/com/example/info_3/MainActivity7.java
@@ -2,13 +2,25 @@ package com.example.info_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity7 extends AppCompatActivity {
-
+    Button buttonvoltar6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
+        buttonvoltar6=findViewById(R.id.buttonvoltar6);
+        buttonvoltar6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity7.this, MainActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
